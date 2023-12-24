e0e360c89d9400f1eb6c64c424220df1a5342746
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-12-24 16:16:27 +0100

    update server

--- server/serve.py
+++ server/serve.py
@@ -1,6 +1,10 @@
 import asyncio
 import os
-import datetime
+import time
+import math
+import typing
+import uuid
+
 import orjson
 
 from concurrent import futures
@@ -8,25 +12,46 @@ from aiohttp import web
 import motor.motor_asyncio
 import pymongo.errors
 import json
+import faulthandler
 
 _dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class DataBase:
 
-    async def __aenter__(self, max_workers: int = 5):
+    async def __aenter__(self, max_workers: int = 10):
         self._client = motor.motor_asyncio.AsyncIOMotorClient()
         self._db = self.client["fingerprints"]
-        self._entries = self.db["entries"]
-        self._stats = self.db["stats"]
+        self._entries = self.db["a_entries"]
+
+        self._paths = self.db["a_paths"]
+        self._bots = self.db["bots"]
+        self._windows = self.db["windows"]
+        self._linux = self.db["linux"]
+        self._ios = self.db["ios"]
+        self._android = self.db["android"]
+        self._mac = self.db["mac"]
+        self._other = self.db["other"]
+
+        self._ips = self.db["ips"]
         try:
             await self.db.validate_collection("entries")  # Try to validate a collection
         except pymongo.errors.OperationFailure:  # If the collection doesn't exist
-            await self.entries.create_index("ip", unique=True, name="ip", )
-            await self.stats.insert_one({})
+            await self.entries.create_index("cookie", unique=True, name="cookie")
+            await self.ips.create_index("ip", unique=True, name="ip")
+
+            await self.paths.create_index("path", unique=True, name="paths")
+            await self.windows.create_index("path", unique=True, name="paths")
+            await self.bots.create_index("path", unique=True, name="paths")
+            await self.linux.create_index("path", unique=True, name="paths")
+            await self.ios.create_index("path", unique=True, name="paths")
+            await self.android.create_index("path", unique=True, name="paths")
+            await self.mac.create_index("path", unique=True, name="paths")
+            await self.other.create_index("path", unique=True, name="paths")
+
         self._loop = asyncio.get_running_loop()
         self._pool = futures.ThreadPoolExecutor(max_workers=max_workers)
-        await self.entries.create_index("ip", unique=True, name="ip", )
+        self._paths_map = {}
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
@@ -39,50 +64,116 @@ class DataBase:
     async def _dump_json(self, data) -> bytes:
         return await self._loop.run_in_executor(self._pool, lambda: orjson.dumps(data))
 
-    async def add_fp_entry(self, ip: str, fp: bytes):
+    async def _find_and_modify(self, collection: str, **kwargs):
+        res = await self.db.command(
+            'findAndModify',
+            collection,
+            **kwargs
+        )
+        value_doc = res.get("value")
+        if value_doc:
+            return value_doc
+        elif res.get("lastErrorObject", {}).get("upserted"):
+            return {"_id": res["lastErrorObject"]["upserted"]}
+        else:
+            raise Exception(res["lastErrorObject"])
+
+    async def add_fp_entry(self, ip: str, cookie: str, fp: bytes):
+        _time = math.floor(time.time())
+        ip_doc = await self.ips.find_one({"ip": ip})
+        collections = [self.paths]
+        if ip_doc:
+            if ip_doc["flag"] > 10:
+                return
+            timestamps: typing.List[int] = ip_doc["timestamps"]
+            for idx, stamp in enumerate(timestamps):
+                if stamp < (_time - 3_600):  # 60s*60min => 1h
+                    timestamps.pop(idx)
+            await self.ips.update_one({"ip": ip}, {"$push": {"timestamps": _time}})
+            if len(timestamps) > 10:
+                await self.ips.update_one({"ip": ip}, {"$inc": {"flag": 1}})
+                return
+        else:
+            try:
+                await self.ips.insert_one(
+                    {"ip": ip, "timestamps": [_time], "flag": 0})
+            except pymongo.errors.DuplicateKeyError:
+                pass
+
         fp = await self._load_json(fp)
+
+        platform = fp["HighEntropyValues"]["platform"]
+        mobile = fp["HighEntropyValues"]["mobile"]
+        is_bot = fp["is_bot"]
+
+        if is_bot:
+            collections.append(self.bots)
+        elif mobile:
+            if platform in ["Android", "null","Linux", "Linux aarch64",
+                            "Linux armv5tejl", "Linux armv6l", "Linux armv7l", "Linux armv8l"]:
+                collections.append(self.android)
+            elif platform in ["iPhone", "iPod", "iPad"]:
+                collections.append(self.ios)
+            else:
+                collections.append(self.other)
+            collections.append(self.other)
+        elif platform in ["OS/2", "Pocket PC", "Windows", "Win16", "Win32", "WinCE"]:
+            collections.append(self.windows)
+        elif platform in ["Macintosh", "MacIntel", "MacPPC", "Mac68K"]:
+            collections.append(self.mac)
+        elif platform in ["Linux", "Linux aarch64","Linux armv5tejl", "Linux armv6l", "Linux armv7l", "Linux armv8l",
+                          "Linux i686", "Linux i686 on x86_64", "Linux ppc64", "Linux x86_64"]:
+            collections.append(self.linux)
+        else:
+            collections.append(self.other)
+
         try:
-            await self.entries.insert_one({"ip": ip, "fp": fp, "timestamp": datetime.datetime.utcnow()})
-            stats = await self.stats.find_one({})
-            stats = await self._loop.run_in_executor(self._pool, lambda: self.update_stats(entry=fp, stats=stats))
-            await self.stats.find_one_and_replace({},stats)
+            await self.entries.insert_one(
+                {"ip": ip, "cookie": cookie, "fp": fp, "timestamp": _time})
         except pymongo.errors.DuplicateKeyError:
             pass
+        else:
+            cors = await self._loop.run_in_executor(self._pool, lambda: self.parse_paths(collections, fp))
+            await asyncio.gather(*cors)
 
-    def update_stats(self, entry, stats=None, path: str = ""):
-        if stats is None:
-            stats = {}
+    def parse_paths(self, collections: typing.List[motor.motor_asyncio.AsyncIOMotorCollection],
+                    entry, path: str = None) -> list:
+        if path is None:
+            path = ""
+        else:
+            path += "."
+        cors = []
         for key, value in entry.items():
             if value is not None:
                 serialized_key = json.dumps(key)
-                if path:
-                    path += "."
                 current_path = path + serialized_key
                 _type = type(value)
 
                 if _type in [int, float, str, bool]:
-                    stats_key = current_path + "." + json.dumps(value)
-                    if stats_key not in stats:
-                        stats[stats_key] = 1
-                    else:
-                        stats[stats_key] += 1
-                elif _type is dict:
-                    stats = self.update_stats(value, stats, current_path)
+                    cors.append(self._on_path(collections, current_path, [value], is_list=False))
                 elif _type is list:
-                    for item in value:
-                        sub_key = current_path + ".[" + json.dumps(item) + "]"
-                        if sub_key not in stats:
-                            stats[sub_key] = 1
-                        else:
-                            stats[sub_key] += 1
-                        if type(item) is dict:
-                            stats = self.update_stats(item, stats, current_path)
+                    cors.append(self._on_path(collections, current_path, value, is_list=True))
+                elif _type is dict:
+                    cors.extend(self.parse_paths(collections, value, current_path))
                 else:
                     raise ValueError(f"Unsupported type: {type(value)}")
+        return cors
 
-        return stats
-
-
+    async def _on_path(self, collections: typing.List[motor.motor_asyncio.AsyncIOMotorCollection], path: str,
+                       values: typing.List[typing.Union[list, str, int, float, bool]],
+                       is_list: bool = False):
+        for collection in collections:
+            try:
+                await collection.insert_one({"path": path, "is_list": is_list})
+            except pymongo.errors.DuplicateKeyError:
+                pass
+        for value in values:
+            value = json.dumps(value).replace(".", "\uFF0E").replace("$", '\uFF04')
+            for collection in collections:
+                await collection.update_one(
+                    {"path": path},
+                    {"$inc": {f"values.{value}": 1}}
+                )
 
     @property
     def client(self) -> motor.motor_asyncio.AsyncIOMotorClient:
@@ -97,12 +188,43 @@ class DataBase:
         return self._entries
 
     @property
-    def stats(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
-        return self._stats
+    def paths(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._paths
+
+    @property
+    def windows(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._windows
+
+    @property
+    def linux(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._linux
+
+    @property
+    def ios(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._ios
+
+    @property
+    def mac(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._mac
+
+    @property
+    def android(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._android
+
+    @property
+    def bots(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._bots
+
+    @property
+    def other(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._other
+
+    @property
+    def ips(self) -> motor.motor_asyncio.AsyncIOMotorCollection:
+        return self._ips
 
 
 class Server:
-    routes = web.RouteTableDef()
 
     def __init__(self):
         pass
@@ -117,9 +239,11 @@ class Server:
 
     def run(self):
         app = web.Application()
-        app.add_routes(self.routes)
         app.add_routes([
-            web.static('/', f"{_dir}/files", ),
+            web.get("/", self.root),
+            web.get("/iframe.html", self.iframe),
+            web.get("/example_page.html", self.example_page),
+            web.get("/bundle.js", self.bundle),
             web.post('/api/v1/logger', self.api_log)
         ])
 
@@ -127,16 +251,29 @@ class Server:
         app.on_startup.append(self._init)
         web.run_app(app, host="0.0.0.0")
 
-    # noinspection PyMethodParameters
-    @routes.get("/")
-    async def root(request: web.BaseRequest):
+    async def root(self, request: web.BaseRequest):
         raise web.HTTPFound('example_page.html')
+
+    @staticmethod
+    async def bundle(request: web.BaseRequest):
+        return web.FileResponse(f"{_dir}/files/bundle.js")
+
+    @staticmethod
+    async def example_page(request: web.BaseRequest):
+        return web.FileResponse(f"{_dir}/files/example_page.html")
+
+    @staticmethod
+    async def iframe(request: web.BaseRequest):
+        response = web.FileResponse(f"{_dir}/files/iframe.html")
+        if not request.cookies.get("driverless-fp-collector"):
+            response.set_cookie("driverless-fp-collector", uuid.uuid4().hex)
+        return response
 
     async def api_log(self, request: web.BaseRequest):
         data = await request.read()
         ip = request.remote
-        await self.db.add_fp_entry(ip, data)
-
+        cookie = request.cookies.get("driverless-fp-collector")
+        await self.db.add_fp_entry(ip, cookie, data)
         return web.Response(text='OK')
 
     @property
@@ -145,5 +282,6 @@ class Server:
 
 
 if __name__ == "__main__":
+    faulthandler.enable()
     server = Server()
     server.run()
